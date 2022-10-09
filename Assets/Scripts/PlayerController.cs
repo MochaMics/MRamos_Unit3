5f1e8262abdf4190f6f4e544b8c6e8ff49eae04a
MochaMics <Mico.o.Ramos@gmail.com>
2022-10-08 22:44:04 -0700

    Initial commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rbPlayer;
+    public float gravityModifier;
+    public float jumpForce;
+    private bool onGround = true;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        rbPlayer = GetComponent<Rigidbody>();
+        Physics.gravity *= gravityModifier;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        bool spaceDown = Input.GetKeyDown(KeyCode.Space);
+        if(spaceDown && onGround)
+        {
+            rbPlayer.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            onGround = false;
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        onGround = true;
+    }
+
+}
