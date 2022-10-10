ea8962936cea85f899f0de7ec1cf7f4ef454032d
MochaMics <Mico.o.Ramos@gmail.com>
2022-10-09 19:01:16 -0700

    Unit 3 Lesson B

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,6 +8,7 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public float jumpForce;
     private bool onGround = true;
+    public bool gameOver = false;
     
     // Start is called before the first frame update
     void Start()
@@ -22,14 +23,23 @@ public class PlayerController : MonoBehaviour
         bool spaceDown = Input.GetKeyDown(KeyCode.Space);
         if(spaceDown && onGround)
         {
-            rbPlayer.AddForce(Vector3.up * 10, ForceMode.Impulse);
+            rbPlayer.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             onGround = false;
+            
         }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        onGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            onGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle"))
+        {
+            Debug.Log("Game Over, bro...");
+            gameOver = true; 
+        }
     }
 
 }
